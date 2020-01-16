aee20fbaeefd93a19660ab7d93b8503da18c5888
11851 <11851@2023NapolitanoT.local>
2020-01-16 10:50:55 -0500

    update to misc

--- R6 Cheats/Misc.cpp
+++ R6 Cheats/Misc.cpp
@@ -10,6 +10,7 @@ void misc()
 
 	WPM(0x05F7A630, { 0x0, 0x28, 0x0, 0xC8, 0xCC }, num = 999999999); //Infinite ammo
 	WPM(0x52ae868, { 0x68, 0x0, 0x28, 0x30, 0x30, 0x28, 0x40 }, num = 0); //No flash
+	WPM(0x52ae868, { 0x68, 0x0, 0x28, 0x30, 0x30, 0x28, 0x40 }, num = 0); //Spoof Spectate
 
 	//Chams
 	WPM(ADDRESS_GLOWMANAGER, { 0xb8, 0x110 }, num2 = 139);  //Red
@@ -21,13 +22,15 @@ void misc()
 
 	while (true) 
 	{
-		if (GetKeyState(VK_NUMPAD1) & 0x8000)
+		if (GetKeyState(VK_NUMPAD1) & 0x8000 && toggle==0)
 		{
 			WPM(0x52ae858, { 0xF8, 0x8, 0x560 }, num2 = 0.0f); //No clip on
+			toggle = 1;
 		}
-		else 
+		else if(toggle==1)
 		{
 			WPM(0x52ae858, { 0xF8, 0x8, 0x560 }, num2 = 0.0f); //No clip off
+			toggle = 0;
 		}
 	}
 }
